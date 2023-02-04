8f8f202d75fdb6feeeb4babd249e0135112496ab
ZopfGg <x.egea.es@gmail.com>
2023-02-04 19:53:22 +0100

    Auto stash before merge of "develop" and "origin/develop"

---
+++ Assets/Cosas de animacion/Animacion.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Animacion : MonoBehaviour
+{
+    public Animator anim;
+    private float inputH;
+    private float inputV;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+    }
+}
