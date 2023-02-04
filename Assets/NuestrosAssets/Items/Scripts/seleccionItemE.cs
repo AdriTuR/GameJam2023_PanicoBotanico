6fa7c660841a42d6145526bb18ee93b0015e90b3
ZopfGg <x.egea.es@gmail.com>
2023-02-04 19:52:18 +0100

    ArregladoSelectorItems

--- Assets/NuestrosAssets/Items/Scripts/seleccionItemE.cs
+++ Assets/NuestrosAssets/Items/Scripts/seleccionItemE.cs
@@ -9,6 +9,7 @@ public class seleccionItemE : MonoBehaviour
     public string[] listaItems = new string[] { "itemPala", "itemRegadera", "itemAbono" };
     public int itemAparecer;
     private int itemQ;
+    private bool hasRun = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,22 +25,27 @@ public class seleccionItemE : MonoBehaviour
         seleccionItem = GameObject.Find("seleccionItemE");
         itemE.transform.position = seleccionItem.transform.position;
         itemE.SetActive(false);
+      
+       
         seleccionItem.SetActive(false);
 
     }
 
     private void OnEnable()
     {
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isActiveAndEnabled)
+        if (isActiveAndEnabled && hasRun == false)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+                Debug.Log("¡¡¡¡¡Activado objeto E!!!!!!");
                 itemE.SetActive(true);
+                hasRun = true;
             }
         }
     }
