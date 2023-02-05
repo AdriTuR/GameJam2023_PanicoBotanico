28a0ceba00255351be30d6367b616ebd6cc25db6
ZopfGg <x.egea.es@gmail.com>
2023-02-05 02:35:52 +0100

    ManagementItems

--- Assets/NuestrosAssets/Items/Scripts/managerItems.cs
+++ Assets/NuestrosAssets/Items/Scripts/managerItems.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class managerItems : MonoBehaviour
 {
-    public string[] listaItems = new string[] { "itemPala", "itemRegadera", "itemAbono" };
-
+    //public string[] listaItems = new string[] { "itemPala", "itemRegadera", "itemAbono" };
+    GameObject[] todosItems;
     private void Awake()
-    {/*
+    {
+        todosItems = GameObject.FindGameObjectsWithTag("Items");
+
+        /*
         for (int i = 0; i < listaItems.Length; i++)
         {
             GameObject item = GameObject.Find(listaItems[i]);
@@ -17,12 +20,15 @@ public class managerItems : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
+        foreach (GameObject go in todosItems)
+        {
+            go.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
